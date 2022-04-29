a235088fd64f3d65d89d2d244e9fc8ec26930440
Amera Alleyne <amera.alleyne@gmail.com>
2022-04-29 01:49:40 -0700

    refetch tweets on submission/ css styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,85 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const { format } = timeago;
+const maxChar = 140;
 
+$(document).ready(function () {
+
+  const loadTweets = function () {
+    $.get("/tweets")
+      .then(function (data) {
+        renderTweets(data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  };
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const tweetTxt = $.trim($("#tweet-text").val());
+
+    if (tweetTxt === "") {
+      $(".error-message").text("Error Tweet field empty");
+      return;
+    }
+    if (tweetTxt.length > maxChar) {
+      $(".error-message").text("Character Limit exceeded");
+    return;
+    }
+    $.post("/tweets", $(this).serialize())
+    .then((data) => {
+      loadTweets();
+    })
+  });
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      $("#card-container").prepend(createTweetElement(tweet));
+    }
+  }
+  //document.getElementById("btn").addEventListener("click", renderTweets);
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  // const someString = "<script>alert('hi!');</script>";
+  // $("form.someClass").text(someString);
+  // const escape =  $("<form>").text(someString).html();
+
+  const createTweetElement = function (tweet) {
+    let timeago = format(tweet.created_at);
+    let name = tweet.user.name;
+    let image = tweet.user.avatars;
+    let handle = tweet.user.handle;
+    let content = tweet.content.text;
+    let $tweet = `
+<article class="card ">
+<div>
+<span class="right">${handle}</span>
+<span> <img class="mb-5" src=${image}></span>
+<span>${name}</span>
+</div>
+<div class="mb-5">
+  <p>${content}</p>
+  <footer>
+    <div class="line"/>
+    <div class="footer-trio">
+      <div>
+       ${timeago}
+      </div>
+      <div>
+        <span><i class="fa-solid fa-flag icon-height"></i></span>
+        <span><i class="fa-solid fa-retweet icon-height"></i></span>
+        <span><i class="fa-regular fa-heart icon-height"></i></span>
+      </div>
+    </div>
+  </footer>
+</div>
+</article>
+`;
+    return $tweet;
+  }
+
+  loadTweets();
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,22 @@
+
 $(document).ready(function() {
   $(".btn").on('click', function() {
     const inputText = $("#tweet-text").val();
-    alert(inputText.length); //The this keyword is a reference to the button
+     //The this keyword is a reference to the button
   });
   $("#tweet-text").on('keyup', function() {
     const inputText = $("#tweet-text").val();
     let wordCount = inputText.length;
      const maxValue = 140;
      let results = maxValue - wordCount;
-     let counter = $("#counter").val(results);
+     let $counter = $("#counter").val(results);
+     console.log(results);
      // let counter = $("#counter").val(results);
      if (results <= -1) {
-      $("#counter").addClass("red");
-     } 
-
-  });
-
+      $counter.addClass("red");
+    }
+    if (results >= 0) {
+      $counter.removeClass("red");
+    }
+    });
 });
